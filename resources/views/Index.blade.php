1b70a49869e9eb79696782b32dced9336e218a3f
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-22 11:56:43 +0200

    Cambiare titolo homepage

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/scss/app.scss', 'resources/js/app.js')
-    <title>laravel-comics</title>
+    <title>@yield('title-page', 'Index')</title>
 </head>
 <body>
     <h1>Hello World</h1>
